debcf5a988fa7160638d564c0cb36c1a3113d125
yfx <1793185558@qq.com>
2023-08-03 13:59:15 +0800

    update

--- backend/custom-go/global/onResponse.go
+++ backend/custom-go/global/onResponse.go
@@ -8,6 +8,7 @@ import (
 
 func OnOriginResponse(hook *base.HttpTransportHookRequest, body *plugins.HttpTransportBody) (*base.ClientResponse, error) {
 	Wg.Add(1)
+	//获取处理完的code
 	go func() {
 		if strconv.Itoa(body.Response.StatusCode) != "" {
 			Mq <- strconv.Itoa(body.Response.StatusCode)
