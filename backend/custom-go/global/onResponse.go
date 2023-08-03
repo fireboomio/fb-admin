d7ac5a1ebe6ef1592dff89b0e0f19d9038712fb1
yfx <1793185558@qq.com>
2023-08-03 16:58:18 +0800

    update

--- backend/custom-go/global/onResponse.go
+++ backend/custom-go/global/onResponse.go
@@ -7,7 +7,7 @@ import (
 )
 
 func OnOriginResponse(hook *base.HttpTransportHookRequest, body *plugins.HttpTransportBody) (*base.ClientResponse, error) {
-	//
+
 	Wg.Add(1)
 	go func() {
 		if strconv.Itoa(body.Response.StatusCode) != "" {
